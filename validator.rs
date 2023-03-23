fa9ef9250460e51818ba2c479c24717da056528f
aasavravi <aasavravi@gmail.com>
2023-03-23 16:18:26 +0530

    Update validator.rs

--- validator.rs
+++ validator.rs
@@ -1,1 +1,8 @@
+const PENALTY_PERCENTAGE: u64 = 10; // 10%
+
 struct Validator { address: Address, stake: u64, } struct Nominator { address: Address, stakes: Vec<Validator>, }
+
+fn penalize_validator(validator: &mut Validator) {
+    // reduce the validator's stake by a fixed percentage
+    validator.stake = validator.stake - (validator.stake * PENALTY_PERCENTAGE / 100);
+}
